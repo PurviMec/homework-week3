219fcd54ce1cec3e8b54906b95559f63e3a21bf3
PurviMec <princy.mecwan94@gmail.com>
2021-09-11 12:30:49 -0400

    resolve errors in alert window

--- assets/script.js
+++ assets/script.js
@@ -1,58 +1,50 @@
 // Assignment code here
 var onClick = function () {
-    // Alert users about password requirements
-    var promptPassword = window.prompt("Please follow the Instructions to generate valid and secure password. Please answer 'yes' or 'no' to continue process for secure and valid password.");
-    
-    if (promptPassword === "" || promptPassword === null) {
-        window.alert("WARNING!!Please enter valid answer.");
-        return onClick();
-    }
+  // Alert users about password requirements
+  var promptPassword = window.prompt("Please follow the Instructions to generate valid and secure password. Please answer 'yes' or 'no' to continue process for secure and valid password.");
+  
+  if (promptPassword === "" || promptPassword === null) {
+      window.alert("WARNING!!Please enter valid answer.");
+      return onClick();
+  }
 
-    if (promptPassword ==="no") {
-        var confirmRetreat = window.confirm("Are you sure you don't want to set password?"); 
+  if (promptPassword ==="no") {
+      var confirmRetreat = window.confirm("Are you sure you don't want to set password?"); 
 
-        //if yes (true), leave window
-        if (confirmRetreat) {
-            window.alert("User decided not to continue");
-            return true;
-        }
-        
-        }
+      //if yes (true), leave window
+      if (confirmRetreat) {
+          window.alert("User decided not to continue");
+          return true;
+      }
+      
+      }
 
-        if (promptPassword === "yes"){
-            var passwordConditions = window.prompt("Please choose password between 8 to 128 characters. It must have ,one special character', 'one capital-character' and 'one numeric character");
-        }
-
-        if (promptPassword.value==" ||textLength<8 || textLength>128") {
-            window.alert("Please take another look at Password-Requirements");
-            return false;
-        
-        }
-        if (promptPassword.search(/[0-9]/)==-1) {
-            window.alert("Please write password which has atleast one Numeric Character");
-            return false;
-        }
-
-
-        if (promptPassword.search(/[A-Z]/)==-1) {
-            window.alert("Please include atleast one Upper Case Character in your password.");
-            return false;
-        }
-
-       if (promptPassword.search(/[!@#$%^&*]/)==-1) {
-            window.alert("Please include atleast one Special Character in your password.");
-            return false;
-        }
-        
-        else {
-          window.alert("Congratulations! You've set your password!");
-          
+      if (promptPassword === "yes"){
+          var passwordConditions = window.prompt("Please choose password between 8 to 128 characters. It must have ,one special character', 'one capital-character' and 'one numeric character");
         
       }
-        return false;
 
-        
+      if (
+          (passwordConditions.length<8) ||
+          (passwordConditions.length>128) ||
+          (passwordConditions.search(/[0-9]/)==-1) ||
+          (passwordConditions.search(/[A-Z]/)==-1) ||
+          (passwordConditions.search(/[!@#$%^&*]/)==-1) ){
+               window.alert("warning!! Please check password requirements again");
+               return onClick();
+      }
+      
+      else {
+          var answer = alert("Your Password is " + passwordConditions);
+          
+      }
 };
+
+
+  
+
+
+  
 
 
     
